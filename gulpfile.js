f3ca3af0849ce21074132e615ae8e2063158385d
dontry <mccoy018@gmail.com>
2017-08-07 16:52:48 +1000

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,110 @@
+/*
+ * @Author: dontry
+ * @Date:   2017-08-06 22:20:00
+ * @Last Modified by:   dontry
+ * @Last Modified time: 2017-08-07 13:18:38
+ */
+
+'use strict';
+
+let gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    compass = require('gulp-compass'),
+    connect = require('gulp-connect'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    minifyHTML = require('gulp-minify-html'),
+    path = require('path');
+
+let env,
+    jsSrc,
+    sassSrc,
+    htmlSrc,
+    fontSrc,
+    cssSrc,
+    outputDir,
+    sassStyle;
+
+env = 'dev';
+
+if (env === 'dev') {
+    outputDir = 'builds/dev/';
+    sassStyle = 'expanded';
+} else {
+    outputDir = 'builds/dist/';
+    sassStyle = 'compressed'
+}
+
+jsSrc = [
+    'scripts/TweenMax.min.js',
+]
+
+sassSrc = ['styles/style.scss'];
+htmlSrc = ['*.html'];
+fontSrc = ['assets/fonts/*'];
+cssSrc = ['styles/*.css'];
+
+gulp.task('js', function() {
+    gulp.src(jsSrc)
+        .pipe(concat('script.js'))
+        .on('error', gutil.log)
+        .pipe(gulpif(env === 'production', uglify()))
+        .pipe(gulp.dest(outputDir + 'scripts'))
+        .pipe(connect.reload())
+});
+
+gulp.task('compass', function() {
+    gulp.src(sassSrc)
+        .pipe(compass({
+                sass: 'styles',
+                css: outputDir + 'styles',
+                image: outputDir + 'assets/img',
+                style: sassStyle,
+                require: ['susy', 'breakpoint']
+            })
+            .on('error', gutil.log))
+        //    .pipe(gulp.dest( outputDir + 'css'))
+        .pipe(connect.reload())
+});
+
+gulp.task('watch', function() {
+    gulp.watch(jsSrc, ['js']);
+    gulp.watch(['styles/*.scss', 'styles/*/*.scss', 'styles/*.css'], ['compass']);
+    gulp.watch('*.html', ['html']);
+});
+
+gulp.task('connect', function() {
+    connect.server({
+        root: outputDir,
+        livereload: true
+    });
+});
+
+gulp.task('html', function() {
+    gulp.src('*.html')
+        .pipe(gulpif(env === 'dist', minifyHTML()))
+        .pipe(gulp.dest(outputDir))
+        .pipe(connect.reload())
+});
+
+gulp.task('icon', function() {
+    gulp.src(fontSrc)
+        .pipe(gulp.dest(outputDir + 'assets/fonts'))
+});
+
+gulp.task('css', function() {
+    gulp.src(cssSrc)
+        .pipe(gulp.dest(outputDir + 'styles'))
+})
+
+// Copy images to dist
+gulp.task('move', function() {
+    gulp.src('assets/img/**/*.*')
+        .pipe(gulpif(env === 'dev', gulp.dest(outputDir + 'assets/img')))
+        .pipe(gulpif(env === 'dist', gulp.dest(outputDir + 'assets/img')))
+});
+
+gulp.task('default', ['watch', 'html', 'js', 'compass', 'icon', 'css',
+    'move', 'connect'
+]);
